068fe8e023adaddc8907e2c4ada9651fc3bf5386
ummehafsa12 <155570557+ummehafsa12@users.noreply.github.com>
2024-03-20 17:09:20 +0000

    Commit by chance

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "umme hafsa",
+  title: "grapic Designer & Content Creator",
+  email: "hafsa@example.com",
+  gitHub: "microsoft beta",
+  instagram: "ummehafsa123",
+  linkedIn: "ummehafsa",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "Codee",
 };
 
 const primaryColor = "#4E567E";
